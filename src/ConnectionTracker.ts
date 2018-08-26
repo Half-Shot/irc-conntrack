d8c39a2468d02ddb298519152406e0acd1ece5fa
Will Hunt <will@half-shot.uk>
2018-08-26 17:21:33 +0100

    Add ability to create IRC connections

--- src/ConnectionTracker.ts
+++ src/ConnectionTracker.ts
@@ -1,14 +1,42 @@
-import { ICConfig } from "./Config";
+import { Config } from "./Config";
 import { WebsocketHandler } from "./WebsocketHandler";
+import { IConnectionState } from "./Rest/ConnectionsResponse";
+import { IrcClient, IrcConnectionOpts } from "./Irc/IrcClient";
+import { SocketConstructorOpts, SocketConnectOpts, TcpSocketConnectOpts } from "net";
+import * as Uuid from "uuid/v4";
+import { IErrorResponse, ERRCODES } from "./Rest/ErrorResponse";
+import { Log } from "./Log";
+
+const log = new Log("ConnTrack");
 
 export class ConnectionTracker {
-    private ircClients: Map<string,any>;
+    private ircClients: Map<string,IrcClient>;
 
-    constructor(private config: ICConfig, private wsHandler: WebsocketHandler) {
+    constructor(private config: Config, private wsHandler: WebsocketHandler) {
         this.ircClients = new Map();    
     }
 
-    public getConnectionsForServer(server: string, detail: string): any[] {
-        return [];
+    public getConnectionsForServer(server: string, detail: string): IConnectionState[] | String[] {
+        log.verbose(`Fetching connections for ${server}`);
+        if (detail === "ids") {
+            return [...this.ircClients.keys()];
+        }
+        return [] as IConnectionState[];
+    }
+
+    public openConnection(serverName: string, opts: IrcConnectionOpts): Promise<string> {
+        const server = this.config.serverConfig(serverName);
+        if (server === undefined) {
+            log.warn(`Connection was requested for unknown server ${serverName}`);
+            return Promise.reject(
+                {error: "Server is not in config", errcode: ERRCODES.notInConfig} as IErrorResponse
+            );
+        }
+        const uuid = Uuid();
+        const client = new IrcClient(uuid, opts);
+        return client.initiate(serverName, server).then(() => {
+            this.ircClients.set(uuid, client);
+            return uuid;
+        });
     }
 }
--- src/Irc/IrcClient.ts
+++ src/Irc/IrcClient.ts
@@ -1,6 +1,58 @@
-export class IrcClient {
-    constructor() {
-       const i = 1 + 1;
+import { Socket, SocketConnectOpts, SocketConstructorOpts } from "net";
+import { ConfigServer } from "../Config";
+import { Log } from "../Log";
+import { TcpSocketConnectOpts } from "net";
+import { ERRCODES, IErrorResponse} from "../Rest/ErrorResponse";
+
+export interface IrcConnectionOpts {
+    nicknames: string | string[],
+    connectionTimeout: number,
+}
+
+export class IrcClient extends Socket {
+    private log: Log;
+
+    constructor(private uuid: string, private ircOpts: IrcConnectionOpts, opts?: SocketConstructorOpts) {
+       super(opts);
+       this.log = new Log("Cli#"+this.uuid.substr(0,12));
+    }
+
+    public initiate(serverName: string, server: ConfigServer) : Promise<undefined> {
+        this.log.info(`Creating new connection for ${serverName}`);
+        const address = server.addressTuple[0];
+        const socketConnectOpts: TcpSocketConnectOpts = {
+            port: address.port,
+            host: address.host,
+        };
+        return new Promise((resolve, reject) => {
+            this.log.verbose(`Connecting to ${address.host}:${address.port}`);
+            const timeout = setTimeout(() => {
+                this.log.warn(`imed out waiting for connection to ${address.host}:${address.port}`);
+                this.destroy(new Error("Timeout waiting for connection"));
+                reject(
+                    {error: "Timed out connecting", errcode: ERRCODES.timeout} as IErrorResponse
+                );
+            }, this.ircOpts.connectionTimeout || 10000);
+            this.once("error", (err) => {
+                this.log.warn(`Error connecting:`, err);
+                this.destroy();
+                reject(
+                    {error: err.message, errcode: ERRCODES.genericFail} as IErrorResponse
+                );
+                clearTimeout(timeout);
+            });
+            this.connect(socketConnectOpts, () => {
+                clearTimeout(timeout);
+                this.log.info(`Connected.`);
+                this.ircSetup();
+                resolve();
+            });
+            this.log.verbose(`Begun connection.`);
+        });
+    }
+    
+    public ircSetup() {
+        this.write("CONNECT hello!\n");
     }
  }
  

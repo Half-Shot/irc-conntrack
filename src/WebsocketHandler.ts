e9a0e3c000b0f6f7653405e61969245dac2a387f
Will Hunt <will@half-shot.uk>
2018-08-26 17:21:53 +0100

    Websocket Handler

--- src/WebsocketHandler.ts
+++ src/WebsocketHandler.ts
@@ -1,21 +1,31 @@
-import { ICConfig } from "./Config";
+import { Config } from "./Config";
 import * as Ws from "ws";
+import { Log } from "./Log";
+import { EventEmitter } from "events";
 
-export class WebsocketHandler {
+const log = new Log("WebsocketHandler");
+
+export class WebsocketHandler extends EventEmitter{
     private connections: Map<string,Ws>;
 
-    constructor (private config : ICConfig) {
+    constructor (private config : Config) {
+        super();
         this.connections = new Map();
     }
 
     public addConnection(host: string, connection: Ws) {
+        log.info(`New connection from ${host}`);
         if (this.connections.has(host)) {
+            log.warn("Dropping previous connection");
             this.dropConnection(host);
         }
         this.connections.set(host, connection);
         if (this.connections.size === this.config.maximumWebsocketConnections) {
+            log.warn("At connection limit, dropping a connection");
             this.dropConnection(this.connections.keys().next().value);
         }
+
+        this.on("message", this.onIrcMessage.bind(this));
 
         // Bind handlers
         connection.onopen = this.onOpen;
@@ -32,19 +42,25 @@ export class WebsocketHandler {
         conn.terminate();
     }
 
-    private onOpen(event: {target: Ws}) {
-        console.log(`[WsHandler] onOpen ${JSON.stringify(event)}`);
+    private onIrcMessage(event: string, ...args: any[]) {
+        this.connections.forEach((cn) => {
+            cn.send({event, args});
+        })
     }
 
-    private onMessage(event: {data: Ws.Data, type: string, target: Ws}) {
-        console.log(`[WsHandler] onMessage ${JSON.stringify(event)}`);
+    private onOpen(e: {target: Ws}) {
+        log.info(`onOpen url=${e.target.url} state=${e.target.readyState}`);
     }
 
-    private onError(event: {error: any, message: string, type: string, target: Ws}) {
-        console.log(`[WsHandler] onError ${JSON.stringify(event)}`);
+    private onMessage(e: {data: Ws.Data, type: string, target: Ws}) {
+        log.verbose(`onMessage type=${e.type} data=${e.data}`);
     }
 
-    private onClose(event: {wasClean: boolean, code: Number, reason: string, target: Ws}) {
-        console.log(`[WsHandler] onClose ${JSON.stringify(event)}`);
+    private onError(e: {error: any, message: string, type: string, target: Ws}) {
+        log.warn(`onError error=${e.error} type=${e.type} msg=${e.message}`);
+    }
+
+    private onClose(e: {wasClean: boolean, code: Number, reason: string, target: Ws}) {
+        log.info(`onClose code=${e.code} reason=${e.reason} wasClean=${e.wasClean}`);
     }
 }

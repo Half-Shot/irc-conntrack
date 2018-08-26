9727914adbb69c0d0be4f017f1a75bdcc336721d
Will Hunt <will@half-shot.uk>
2018-08-26 17:20:14 +0100

    Add Message Parser

---
+++ src/Irc/IMessage.ts
@@ -0,0 +1,87 @@
+const ircColors = require("irc-colors");
+import {Codes} from "./IrcCodes";
+
+export interface IMessage {
+    prefix?: string,
+    nick?: string,
+    user?: string,
+    host?: string,
+    command?: string,
+    commandType?: string,
+    rawCommand?: string,
+    server?: string,
+    args?: string[],
+}
+
+/**
+ * parseMessage(line, stripColors)
+ *
+ * takes a raw "line" from the IRC server and turns it into an object with
+ * useful keys
+ * @param {String} line Raw message from IRC server.
+ * @param {Boolean} stripColors If true, strip IRC colors.
+ * @return {Object} A parsed message object.
+ */
+export function parseMessage(line: string, stripColors: boolean): IMessage {
+    const message: IMessage = {};
+    let match;
+
+    if (stripColors) {
+        line = ircColors.stripColorsAndStyle(line);
+    }
+
+    // Parse prefix
+    match = line.match(/^:([^ ]+) +/);
+    if (match) {
+        message.prefix = match[1];
+        line = line.replace(/^:[^ ]+ +/, '');
+        match = message.prefix.match(/^([_a-zA-Z0-9\[\]\\`^{}|-]*)(!([^@]+)@(.*))?$/);
+        if (match) {
+            message.nick = match[1];
+            message.user = match[3];
+            message.host = match[4];
+        }
+        else {
+            message.server = message.prefix;
+        }
+    }
+
+    // Parse command
+    match = line.match(/^([^ ]+) */);
+    if (match === null) {
+        throw new Error("Message was not in an understood format");
+    }
+    message.command = match[1];
+    message.rawCommand = match[1];
+    message.commandType = 'normal';
+    line = line.replace(/^[^ ]+ +/, '');
+
+    if (Codes[message.rawCommand]) {
+        message.command     = Codes[message.rawCommand].name;
+        message.commandType = Codes[message.rawCommand].type;
+    }
+
+    message.args = [];
+    var middle, trailing;
+
+    // Parse parameters
+    if (line.search(/^:|\s+:/) != -1) {
+        match = line.match(/(.*?)(?:^:|\s+:)(.*)/);
+        if (match === null) {
+            throw new Error("Message was not in an understood format");
+        }
+        middle = match[1].trimRight();
+        trailing = match[2];
+    }
+    else {
+        middle = line;
+    }
+
+    if (middle.length)
+        message.args = middle.split(/ +/);
+
+    if (typeof (trailing) != 'undefined' && trailing.length)
+        message.args.push(trailing);
+
+    return message;
+}

bac81a3234fdcdb3b1f9261d59a93bd7460f2921
Will Hunt <will@half-shot.uk>
2018-08-26 11:32:43 +0100

    Tests

---
+++ test/Irc/TestIrcClient.ts
@@ -0,0 +1,10 @@
+import { expect } from "chai";
+import { IrcClient } from "../../src/Irc/IrcClient";
+
+describe("IrcClient", () => {
+   describe("constructor", () => {
+      it("should construct", () => {
+         const c = new IrcClient();
+      });
+   });
+});
--- test/TestRestHandler.ts
+++ test/TestRestHandler.ts
@@ -1,10 +1,36 @@
 import { expect } from "chai";
 import { RestHandler } from "../src/RestHandler";
+import * as Mock from 'mock-require';
+
+let callbacks = {} as any;
+
+Mock('express', () => {
+    return {
+        get: (params: string, cb: Function) => {
+            callbacks["get:"+params] = cb;
+        },
+        post: (params: string, cb: Function) => {
+            callbacks["post:"+params] = cb;
+        },
+        listen: (port: number) => {
+
+        }
+    }
+});
+   
 
 describe("RestHandler", () => {
    describe("constructor", () => {
       it("should construct", () => {
-         const c = new RestHandler({} as any, 5);
+         const c = new RestHandler({} as any, {} as any, {} as any);
       });
    });
+   describe("start", () => {
+    it("should setup all handers", () => {
+       const c = new RestHandler({} as any, {} as any, {} as any);
+       c.start();
+       console.log(callbacks);
+       expect(Object.keys(callbacks)).to.have.lengthOf(6);
+    });
+ });
 });

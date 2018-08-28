55f0b38f17012f140be65f9e7203620ba9171c1d
Will Hunt <will@half-shot.uk>
2018-08-28 18:09:23 +0100

    More IrcClient features

--- src/Irc/IrcClient.ts
+++ src/Irc/IrcClient.ts
@@ -4,22 +4,34 @@ import { Log } from "../Log";
 import { TcpSocketConnectOpts } from "net";
 import { ERRCODES, IErrorResponse} from "../Rest/ErrorResponse";
 import { IIrcSupported } from "./IrcSupported";
+import { IrcUtil } from "./IrcUtil";
+import { parseMessage, IMessage } from "./IMessage";
+
+const DEFAULT_CONNECTION_TIMEOUT_MS = 10000;
+const LINE_DELIMITER = new RegExp('\r\n|\r|\n')
+
 
 export interface IrcConnectionOpts {
     nicknames: string | string[],
     connectionTimeout: number,
+    detectEncoding: boolean,
+    stripColors: boolean,
 }
 
 export class IrcClient extends Socket {
     private supported: IIrcSupported;
     private log: Log;
+    private dataBuffer: Buffer;
+    private requestedDisconnect: Boolean = false;
 
     constructor(private uuid: string, private ircOpts: IrcConnectionOpts, opts?: SocketConstructorOpts) {
-       super(opts);
-       this.log = new Log("Cli#"+this.uuid.substr(0,12));
+        super(opts);
+        this.ircOpts.stripColors = !(this.ircOpts.stripColors === false);
+        this.log = new Log("Cli#"+this.uuid.substr(0,12));
         this.supported = {
             casemapping: "",
         };
+        this.dataBuffer = Buffer.alloc(0);
     }
 
     public initiate(serverName: string, server: ConfigServer) : Promise<undefined> {
@@ -38,7 +50,7 @@ export class IrcClient extends Socket {
                 reject(
                     {error: "Timed out connecting", errcode: ERRCODES.timeout} as IErrorResponse
                 );
-            }, this.ircOpts.connectionTimeout || 10000);
+            }, this.ircOpts.connectionTimeout || DEFAULT_CONNECTION_TIMEOUT_MS);
             this.once("error", (err) => {
                 this.log.warn(`Error connecting:`, err);
                 this.destroy();
@@ -50,6 +62,10 @@ export class IrcClient extends Socket {
             this.connect(socketConnectOpts, () => {
                 clearTimeout(timeout);
                 this.log.info(`Connected.`);
+                if (!this.ircOpts.detectEncoding) {
+                    this.setEncoding("utf-8");
+                }
+                this.on("data", this.onData.bind(this));
                 this.ircSetup();
                 resolve();
             });
@@ -59,6 +75,44 @@ export class IrcClient extends Socket {
     
     public ircSetup() {
         this.write("CONNECT hello!\n");
+    }
+
+    private onData(chunk: string|Buffer) {
+        if (typeof (chunk) === 'string') {
+            this.dataBuffer.write(chunk);
+        } else {
+            this.dataBuffer = Buffer.concat([this.dataBuffer, chunk]);
+        }
+
+        const lines = IrcUtil.convertEncoding(
+            this.dataBuffer,
+            this.ircOpts.detectEncoding,
+        ).toString().split(LINE_DELIMITER);
+
+        if (lines.pop()) {
+            // if buffer is not ended with \r\n, there's more chunks.
+            return;
+        } else {
+            // else, initialize the buffer.
+            this.dataBuffer = Buffer.alloc(0);
+        }
+
+        lines.forEach((line) => {
+            if (line.length) {
+                const message = parseMessage(line, this.ircOpts.stripColors);
+                try {
+                    this.onRawMessage(message);
+                } catch (err) {
+                    if (!this.requestedDisconnect) {
+                        throw err;
+                    }
+                }
+            }
+        });
+    }
+
+    private onRawMessage(msg: IMessage) {
+        this.emit("raw", msg);
     }
  }
  

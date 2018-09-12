b75a243033f3394fb8ff06ce4de923c983e2c61a
Will Hunt <will@half-shot.uk>
2018-09-12 23:38:04 +0100

    Allow the mock ircd to send to and track connections

--- test/Mocks/MockIrcd.ts
+++ test/Mocks/MockIrcd.ts
@@ -5,6 +5,7 @@ export class MockIrcd {
     public running: boolean;
     public dataRecieved: string;
     public connections: number;
+    private sockets: Socket[];
 
     public dataCount: number;
     private dataPromiseTrigger: any;
@@ -17,13 +18,10 @@ export class MockIrcd {
         this.connections = 0;
         this.running = false;
         this.dataCount = 0;
+        this.sockets = [];
     }
 
     public spinUp(): Promise<void> {
-        this.dataRecieved = "";
-        this.connections = 0;
-        this.dataCount = 0;
-        this.dataExpecting = -1;
         this.listener.listen(5544, "127.0.0.1");
         return new Promise((resolve, reject) => {
             this.listener.once("error", (err) => {
@@ -48,11 +46,6 @@ export class MockIrcd {
         });
     }
 
-    private onConnection(conn: Socket) {
-        conn.on("data", (data) => {this.onData(data)});
-        this.connections++;
-    }
-
     public waitForData(expectedCount: number, timeout: number): Promise<void> {
         this.dataExpecting = expectedCount;
         return new Promise((resolve, reject) => {
@@ -66,6 +59,23 @@ export class MockIrcd {
                 clearTimeout(tOut);
             };
         });
+    }
+
+    public send(msg: string, index: number = 0): Promise<void> {
+        return new Promise((resolve) => {
+            this.sockets[index].write(msg, () => {
+                resolve();
+            });
+        });
+    }
+
+    private onConnection(conn: Socket) {
+        conn.on("data", (data) => {this.onData(data)});
+        conn.on("close", () => {
+            this.sockets.splice(this.sockets.findIndex((c) => c === conn), 0);
+        });
+        this.sockets.push(conn);
+        this.connections++;
     }
 
     private onData(data: Buffer|string) {

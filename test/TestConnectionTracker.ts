c8613fac1da2cff979ab567e4dd4b38423888de6
Will Hunt <will@half-shot.uk>
2018-09-13 17:57:19 +0100

    Add many more tests to ConnectionTracker

--- test/TestConnectionTracker.ts
+++ test/TestConnectionTracker.ts
@@ -15,7 +15,12 @@ class MockIrcClient {
         /* stub */
     }
 
-    public initiate(): Promise<void> {
+    public initiate() {
+        return Promise.resolve();
+    }
+
+    public send(msg: string) {
+        lastSentMessage = msg;
         return Promise.resolve();
     }
 }
@@ -26,25 +31,25 @@ Mock("../src/Irc/IrcClient",
 });
 
 import { ConnectionTracker } from "../src/ConnectionTracker";
-import { ConfigServer } from "../src/Config";
+import { ConfigServer, Config } from "../src/Config";
 import { IConnectionState } from "../src/Rest/IConnectionsResponse";
+import { IErrorResponse, ERRCODES } from "../src/Rest/IErrorResponse";
 
 let wsHooks: any = {};
+let config: Config;
+let lastSentMessage: string;
 
 const createConnectionTracker = async (createClients: number = 0) => {
     wsHooks = {};
-    const t = new ConnectionTracker({
-        serverConfig: () => {
-            return new ConfigServer({
-                name: "foo",
-                addresses: ["127.0.0.1:1111"],
-            });
-        },
-    } as any, {
-        on: (name: string, func: () => void) => {
-            wsHooks[name] = func;
-        },
-    } as any);
+    config = Config.parseFile("./test/config.sample.yaml");
+    const t = new ConnectionTracker(
+        config,
+        {
+            on: (name: string, func: () => void) => {
+                wsHooks[name] = func;
+            },
+        } as any,
+    );
     for (let i = 0; i < createClients; i++) {
         await t.openConnection("foo", {
             nicknames: `GoodDog#${i}`,
@@ -55,7 +60,7 @@ const createConnectionTracker = async (createClients: number = 0) => {
         });
     }
     return t;
-}
+};
 
 describe("ConnectionTracker", () => {
     describe("constructor", () => {
@@ -89,6 +94,83 @@ describe("ConnectionTracker", () => {
         it("should throw if detail is not supported", async () => {
             const t = await createConnectionTracker();
             expect(() => {t.getConnectionsForServer("foo", "invaliddetail"); }).to.throw();
+        });
+    });
+    describe("openConnection", () => {
+        it("should reject if server is not known", async () => {
+            const t = await createConnectionTracker();
+            try {
+                await t.openConnection("fakeserver", {
+                    nicknames: `BadDog`,
+                    connectionTimeout: -1,
+                    detectEncoding: false,
+                    stripColors: false,
+                    ignoreBadMessages: false,
+                });
+            } catch (e) {
+                expect((e as IErrorResponse).errcode).to.equal(ERRCODES.notInConfig);
+                return;
+            }
+            throw new Error("Expected to throw");
+        });
+        it("should reject if over connection limit", async () => {
+            const MAX_CLIENTS = 5;
+            const t = await createConnectionTracker(MAX_CLIENTS);
+            try {
+                await t.openConnection("foo", {
+                    nicknames: `BadDog`,
+                    connectionTimeout: -1,
+                    detectEncoding: false,
+                    stripColors: false,
+                    ignoreBadMessages: false,
+                });
+            } catch (e) {
+                expect((e as IErrorResponse).errcode).to.equal(ERRCODES.connectionLimit);
+                return;
+            }
+            throw new Error("Expected to throw");
+        });
+    });
+    describe("runCommand", () => {
+        it("should send error if client not found", async () => {
+            const t = await createConnectionTracker();
+            return new Promise((resolve) => {
+                t.runCommand(
+                    {client_id: "aaaa", content: "aaaa", type: "raw", id: "1"},
+                    {
+                        send: (jsonError: string) => {
+                            expect(jsonError).to.be.equal(`{"id":"1","errcode":"IC_CLIENT_NOT_FOUND"}`);
+                            resolve();
+                        },
+                    } as any,
+                );
+            });
+        });
+        it("should send error if the command is not understood", async () => {
+            const t = await createConnectionTracker();
+            return new Promise((resolve) => {
+                t.runCommand(
+                    {client_id: "aaaa", content: "aaaa", type: "badcommand", id: "1"},
+                    {
+                        send: (jsonError: string) => {
+                            expect(jsonError).to.be.equal(`{"id":"1","errcode":"IC_COMMAND_NOT_RECOGNISED"}`);
+                            resolve();
+                        },
+                    } as any,
+                );
+            });
+        });
+        it("should send raw command to client", async () => {
+            const t = await createConnectionTracker(1);
+            const clientId = t.getConnectionsForServer("foo", "ids")[0] as string;
+            return new Promise((resolve) => {
+                t.runCommand(
+                    {client_id: clientId, content: "aaaa", type: "raw", id: "1"},
+                    {} as any,
+                );
+                expect(lastSentMessage).to.equal("aaaa");
+                resolve();
+            });
         });
     });
 });

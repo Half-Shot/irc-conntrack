14a06d930d3879ece2c06a13ec35483e70403725
Will Hunt <will@half-shot.uk>
2018-09-15 09:16:56 +0100

    State original copyright notice

--- src/Irc/MessageParser.ts
+++ src/Irc/MessageParser.ts
@@ -22,6 +22,23 @@ import { ITopic } from "./Messages/ITopic";
  * and abstracting away the state from the client.
  *
  * The original file is https://github.com/matrix-org/node-irc/blob/master/lib/irc.js
+ * Original copyright notice served below:
+ *
+ *   irc.js - Node JS IRC client library
+ *
+ *   (C) Copyright Martyn Smith 2010
+ *
+ *   This library is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU General Public License as published by
+ *   the Free Software Foundation, either version 3 of the License, or
+ *   (at your option) any later version.
+ *   This library is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU General Public License for more details.
+ *
+ *   You should have received a copy of the GNU General Public License
+ *   along with this library.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /**

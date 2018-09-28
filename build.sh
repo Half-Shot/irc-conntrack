11ed0807e2bddd19211fdb015c9d352342967616
Will Hunt <will@half-shot.uk>
2018-09-28 02:02:46 +0100

    New build script!

--- build.sh
+++ build.sh
@@ -1,2 +1,14 @@
-echo "Cloning theme"
-git clone https://github.com/matcornic/hugo-theme-learn ./themes/hugo-theme-learn
+#!/bin/bash
+THEME_DIR="./themes/hugo-theme-learn"
+
+if [[ ! -d $THEME_DIR ]]; then
+  echo "Cloning theme"
+  git clone https://github.com/matcornic/hugo-theme-learn $THEME_DIR
+else
+  echo "Pulling theme"
+  cd $THEME_DIR
+  git pull
+  cd ../..
+fi
+
+hugo
